68ad47883d7a68d028910eb4d112a9aad491568c
Eugene Moiseev <eugen.moiseev@gmail.com>
2019-06-15 02:11:24 +0300

    first commit

---
+++ parser/parser.py
@@ -0,0 +1,44 @@
+import sys
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    r = requests.get(url).text
+    return r
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    article_list = soup.find('body').find_all('p')
+    new_article_list = []
+    pattern = re.compile(r'\w')
+    for p in article_list:
+        if re.findall(pattern, p.text):
+            new_article_list.append(p)
+
+    return new_article_list
+
+
+def replace_link(p_list):
+    for p in p_list:
+        a_text = p.find('a')
+        if a_text is None:
+            print(p.text)
+        else:
+            a_text_href = a_text.get('href')
+            replace_a = '[' + a_text.text + '] (' + a_text_href + ')'
+            a_text.replaceWith(replace_a)
+            print(p.text)
+
+
+def main(url):
+    try:
+        return replace_link(get_data(get_html(url)))
+    except Exception:
+        print('Error! Input correct url please')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
